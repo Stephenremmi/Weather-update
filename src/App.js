d8fa1a8fe237cdf63897ee8e6935d27cd1abebc3
stephen remmi <stephenremmi@gmail.com>
2020-09-08 14:54:19 +0300

    Add assests folder and store jpeg files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
 
+const api = {
+  key:"3a8bda654057edce7a42639fa980abdf",
+  base:"https://api.openweathermap.org/api/data/2.5"
+}
+
 function App() {
   return (
     <div className="app">
